1daad5b2824a6fbece711ccebdfbcdaf8147ee50
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-11-08 14:17:45 +0530

    Update mongodb_update_price.js

--- mongodb_update_price.js
+++ mongodb_update_price.js
@@ -6,7 +6,7 @@ let fetch;
   fetch = fetchModule.default; 
 
   const uri = 'mongodb+srv://liltest:BI6H3uJRxYOsEsYr@cluster0.qtfou20.mongodb.net/';
-  const dbName = 'sample';
+  const dbName = 'backend';
   const apiUrl = 'https://api.geckoterminal.com/api/v2/networks/mantle/tokens/multi/0x201eba5cc46d216ce6dc03f6a759e8e766e956ae%2C0x09bc4e0d864854c6afb6eb9a9cdf58ac190d0df9%2C0xdeaddeaddeaddeaddeaddeaddeaddeaddead1111';
 
   async function fetchAndInsertData() {
@@ -18,14 +18,16 @@ let fetch;
         const client = new MongoClient(uri, { useUnifiedTopology: true });
         await client.connect();
         const database = client.db(dbName);
-        const collection = database.collection('sample1');
+        const collection = database.collection('price_usd');
 
         for (const item of data.data) {
           const { id, attributes: { price_usd } } = item;
 
-          
-          await collection.updateOne({ id }, { $set: { price_usd } }, { upsert: false });
-          console.log(`Updated document with id: ${id}`);
+          // Extract the desired part of the id
+          const extractedId = id.split('_')[1];
+
+          await collection.updateOne({ id: extractedId }, { $set: { price_usd } }, { upsert: true });
+          console.log(`Updated document with id: ${extractedId}`);
         }
 
         client.close();
