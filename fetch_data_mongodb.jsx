44cb2e9e26b6d5b6fa1abc362e3e045393a329dd
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-11-08 13:32:24 +0530

    Update fetch_data_mongodb.jsx

--- fetch_data_mongodb.jsx
+++ fetch_data_mongodb.jsx
@@ -64,6 +64,34 @@ app.get('/price', async (req, res) => {
   }
 });
 
+app.get('/tvl_usd_sum', async (req, res) => {
+  try {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    await client.connect();
+
+    const database = client.db('backend'); 
+    const collection = database.collection('tvl');
+
+    
+    const sumResult = await collection.aggregate([
+      {
+        $group: {
+          _id: null,
+          total: { $sum: '$tvl_usd' },
+        },
+      },
+    ]).toArray();
+
+    client.close();
+
+    res.json({ sum: sumResult[0].total });
+  } catch (error) {
+    console.error('Error calculating TVL sum:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 app.listen(port, '0.0.0.0',() => {
   console.log(`API server is running on port ${port}`);
 });
