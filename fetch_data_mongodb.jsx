c6e2bfcbf35c94e5436bceae6aa6433eb1ede76a
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-11-17 13:33:45 +0530

    Update fetch_data_mongodb.jsx

--- fetch_data_mongodb.jsx
+++ fetch_data_mongodb.jsx
@@ -234,6 +234,23 @@ app.get('/xriv/users/:walletAddress', async (req, res) => {
   }
 });
 
+app.get('/vault/stats', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = client.db('backend');
+    const collection = db.collection('stats');
+
+    const data = await collection.find({}).toArray();
+
+    client.close();
+
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data from stats collection:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 app.listen(port, '0.0.0.0',() => {
   console.log(`API server is running on port ${port}`);
 });
