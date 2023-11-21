41cb8a1c532bf5f35f6b506ae426eb67be94ab91
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-11-21 15:52:14 +0530

    Update mongodb_update_price.js

--- mongodb_update_price.js
+++ mongodb_update_price.js
@@ -6,7 +6,7 @@ let fetch;
   fetch = fetchModule.default; 
 
   const uri = 'mongodb+srv://liltest:BI6H3uJRxYOsEsYr@cluster0.qtfou20.mongodb.net/';
-  const dbName = 'backend';
+  const dbName = 'vaults';
   const apiUrl = 'https://api.geckoterminal.com/api/v2/networks/mantle/tokens/multi/0x201eba5cc46d216ce6dc03f6a759e8e766e956ae%2C0x09bc4e0d864854c6afb6eb9a9cdf58ac190d0df9%2C0xdeaddeaddeaddeaddeaddeaddeaddeaddead1111';
 
   async function fetchAndInsertData() {
@@ -18,7 +18,7 @@ let fetch;
         const client = new MongoClient(uri, { useUnifiedTopology: true });
         await client.connect();
         const database = client.db(dbName);
-        const collection = database.collection('price_usd');
+        const collection = database.collection('coingecko');
 
         for (const item of data.data) {
           const { id, attributes: { price_usd } } = item;
