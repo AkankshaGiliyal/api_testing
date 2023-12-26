a468eef3af1a51d22f7383ecb3d90042f679c9cf
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-12-26 15:30:26 +0530

    Update dex_script.jsx

--- dex_script.jsx
+++ dex_script.jsx
@@ -11,38 +11,26 @@ async function updateStats() {
 
     const database = client.db(dbName);
     const statsCollection = database.collection('stats');
-    const tvlCollection = database.collection('mantle');
-    const tvlMantaCollection = database.collection('manta-pacific');
-    const tvlTelosCollection = database.collection('telos');
+    const vaultsCollection = database.collection('vault');
 
     const statsDocuments = await statsCollection.find({}).toArray();
 
     for (const doc of statsDocuments) {
-      const chainNameStats = doc.chain; 
+      const chainNameStats = doc.chain;
       const dex = doc.dex;
 
-      const tvlCount = await tvlCollection.countDocuments({ 'chain': chainNameStats, dex });
-      const tvlMantaCount = await tvlMantaCollection.countDocuments({ 'chain': chainNameStats, dex });
-      const tvlTelosCount = await tvlTelosCollection.countDocuments({ 'chain': chainNameStats, dex });
+      const tvlCount = await vaultsCollection.countDocuments({ 'chain': chainNameStats, dex });
+      const tvlDocuments = await vaultsCollection.find({ 'chain': chainNameStats, dex }).toArray();
 
-      const tvlDocuments = await tvlCollection.find({ 'chain': chainNameStats, dex }).toArray();
-      const tvlMantaDocuments = await tvlMantaCollection.find({ 'chain': chainNameStats, dex }).toArray();
-      const tvlTelosDocuments = await tvlTelosCollection.find({ 'chain': chainNameStats, dex }).toArray();
+      const tvlSum = tvlDocuments.reduce((sum, vaultDoc) => sum + (vaultDoc.tvlUSD || 0), 0);
 
-      const tvlSum = tvlDocuments.reduce((sum, tvlDoc) => sum + (tvlDoc.tvlUSD || 0), 0);
-      const tvlMantaSum = tvlMantaDocuments.reduce((sum, tvlMantaDoc) => sum + (tvlMantaDoc.tvlUSD || 0), 0);
-      const tvlTelosSum = tvlTelosDocuments.reduce((sum, tvlTelosDoc) => sum + (tvlTelosDoc.tvlUSD || 0), 0);
-
-      const totalSum = tvlSum + tvlMantaSum + tvlTelosSum;
-
-      
       await statsCollection.updateOne(
         { chain: chainNameStats, dex },
         {
           $set: {
-            totalVaults: tvlCount + tvlMantaCount + tvlTelosCount, 
-            tvlUSD: totalSum 
-          }
+            totalVaults: tvlCount,
+            tvlUSD: tvlSum,
+          },
         }
       );
     }
@@ -60,4 +48,5 @@ updateStats();
 setInterval(() => {
   console.log('Running the updateStats function...');
   updateStats();
-}, 10 * 60 * 1000);
+}, 30 * 60 * 1000);
+
